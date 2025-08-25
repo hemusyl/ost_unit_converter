5a1a6acf5204ee4fa8f43b23810772f1cf65180d
hemusyl <humayun1278@gmail.com>
2025-08-25 05:40:05 -0400

    added form field

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -8,11 +8,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Unit Converter'),
+        centerTitle: true,
+        backgroundColor: Colors.yellow,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _controller,
+              decoration: InputDecoration(
+                hintText: 'Unit',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            FractionallySizedBox(
+              // width: double.infinity,
+                widthFactor: 0.6,
+                child: ElevatedButton(onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                  ),
+                  child: Text('Convert'),
+                )
+            )
+          ],
+        ),
       ),
     );
   }
