f13c10aa2dd597d61478767f4e2da62a7f129d94
hemusyl <humayun1278@gmail.com>
2025-08-26 16:43:40 -0400

    added dropdown field

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -11,6 +11,29 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final TextEditingController _controller = TextEditingController();
 
+
+  final Map<String, double> convertionTypes  = {
+    'Meters': 1.0,
+    'Kilometers': 0.001,
+    'Centimeters': 100,
+    'Inches': 39.37,
+    'Feet': 3.28084,
+    "Yards": 1.09361,
+    "Miles": 0.000621371,
+  };
+
+  String fromUnit = 'Meters'; /// Set default unit
+  String toUnit = 'Kilometers'; /// Set default unit
+  double inputValue = 0;
+  double result = 0.0;
+
+  void convert() {
+    double valueInMeters = inputValue   / convertionTypes[fromUnit]!;
+    setState(() {
+      result = valueInMeters * convertionTypes [toUnit]!;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +54,41 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DropdownButton<String>(
+                    value: fromUnit,
+                  onChanged: (value) {
+                    setState(() {
+                      fromUnit = value!;
+                    });
+                  },
+                  items: convertionTypes.keys.map((unit) {
+                    return DropdownMenuItem(
+                      value: unit,
+                      child: Text(unit),
+                    );
+                  }).toList(),
+                ),
+                Text('to'),
+                DropdownButton<String>(
+                  value: toUnit,
+                  onChanged: (value) {
+                    setState(() {
+                      toUnit = value!;
+                    });
+                  },
+                  items: convertionTypes.keys.map((unit) {
+                    return DropdownMenuItem(
+                      value: unit,
+                      child: Text(unit),
+                    );
+                  }).toList(),
+                ),
+
+              ],
+            ),
             FractionallySizedBox(
               // width: double.infinity,
                 widthFactor: 0.6,
